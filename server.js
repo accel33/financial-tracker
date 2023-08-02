22731f90fbffb88c828cc1fed3720537ed5f51f2
accel33 <accelwtf@gmail.com>
2023-08-02 18:23:34 -0500

    second

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
+    console.log(`Server started at port ${PORT}`)
 })
